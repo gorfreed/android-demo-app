880b6c4673455ce491f938172a115777aa284075
Andre Brühl <andre.bruehl@dynport.de>
2016-08-04 18:13:16 +0200

    added more example textfields

--- app/src/main/java/com/example/kolja/mytranslatedapplication/MainActivity.java
+++ app/src/main/java/com/example/kolja/mytranslatedapplication/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.kolja.mytranslatedapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Html;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,11 +13,16 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         String helloWorld = String.format(getString(R.string.hello_world), "Andre");
         String pointsGained = String.format(getString(R.string.points_gained), 101);
+        String text = String.format(getString(R.string.with_html));
+        CharSequence withHtml = Html.fromHtml(text);
 
-        TextView view1 = (TextView) findViewById(R.id.textView2);
-        view1.setText(helloWorld);
+        TextView helloWorldView = (TextView) findViewById(R.id.helloWorld);
+        helloWorldView.setText(helloWorld);
 
-        TextView view2 = (TextView) findViewById(R.id.textView3);
-        view1.setText(pointsGained);
+        TextView pointsGainedView = (TextView) findViewById(R.id.pointsGained);
+        pointsGainedView.setText(pointsGained);
+
+        TextView withHtmlView = (TextView) findViewById(R.id.withHtml);
+        withHtmlView.setText(withHtml);
     }
 }
